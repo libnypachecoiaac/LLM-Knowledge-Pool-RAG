81f90adf396928d5d385d88ef7147d88fb7ed12f
libnypachecoiaac <libny.francisco.pacheco@students.iaac.net>
2024-06-02 23:59:01 +0200

    Chats working fine

--- 03_ask_rag.py
+++ 03_ask_rag.py
@@ -4,7 +4,7 @@ import numpy as np
 import json
 from config import *
 
-embeddings_json= "knowledge_pool/brutalism_wikipedia.json"
+embeddings_json= "LLM-Knowledge-Pool-RAG\knowledge_pool/jacobs_text.json"
 
 # Choose between "local" or "openai" mode
 mode = "local" # or "local"
@@ -13,7 +13,7 @@ client, completion_model = api_mode(mode)
 # question = "What is the program for the building?"
 # question = "What is the place like?"
 # question = "Is there any mention of the construction materials that should be used?"
-question = "What are the names of the most famous brutalist buildings?"
+question = "What is urbanism?"
 
 num_results = 1 #how many vectors to retrieve
 
